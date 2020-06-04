53c95cb8f62ae23eba2a5eeca3757bee47022759
Blue-Ranger <53351056+Blue-Ranger@users.noreply.github.com>
2020-06-04 19:03:41 -0400

    added some code getting setState form errors

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React from "react";
+import TodoForm from "./components/TodoForm";
+import "./App.css";
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      items: [],
+      currentItem: {
+        text: "",
+        key: "",
+      },
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.handleInput.bind(this);
+  }
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="App">
+        <h2>Todo App!</h2>
+        <h1>{this.state.message}</h1>
+        <TodoForm />
       </div>
     );
   }
 }
 
 export default App;
+
+// you will need a place to store your state in this component.
+// design `App` to be the parent component of your application.
+// this component is going to take care of state, and any change handlers you need to work with your state
